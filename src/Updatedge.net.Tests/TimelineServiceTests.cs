3fce18afe61f16a0af09c62a74ff6729474751d3
anthonyboffee <anthonyboffee@gmail.com>
2020-02-27 15:56:52 +0000

    TimelineService implemented with unit tests

---
+++ src/Updatedge.net.Tests/TimelineServiceTests.cs
@@ -0,0 +1,326 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Updatedge.net.Services.V1;
+using Flurl.Http.Testing;
+using Updatedge.net.Exceptions;
+using Udatedge.Common;
+using Udatedge.Common.Models;
+using Udatedge.Common.Models.TimelineEvents;
+using Light.GuardClauses.Exceptions;
+
+namespace Updatedge.net.Tests
+{
+    public class TimelineServiceTests
+    {
+        private HttpTest _httpTest;
+        private TimelineService _timelineService;        
+        
+        
+        [SetUp]
+        public void Setup()
+        {
+            // Put Flurl into test mode
+            _httpTest = new HttpTest();
+
+            // instantiate the Availability service
+            _timelineService = new TimelineService("https://localhost/", "1234567890");
+            
+        }
+
+
+        #region GetEvents tests
+        [Test]
+        public async Task GetEvents_OkResult()
+        {
+            // Arrange
+            var okResult = new List<TimelineEvent>
+            {
+                new TimelineEvent
+                {
+                    Id = "1",
+                    CreatedAt = DateTimeOffset.Now.AddDays(-1),
+                    End = DateTimeOffset.Now,
+                    EventType = "",
+                    InstanceId = "Id1",
+                    RepeatInstance = false,
+                    Repeats = false,
+                    RepeatType = "",
+                    RepeatUntil = null,
+                    Start = DateTimeOffset.Now.AddDays(-1),
+                    UpdatedAt = DateTimeOffset.Now.AddDays(-1)
+                }
+            };
+
+            _httpTest.RespondWithJson(okResult);
+            
+            var result = await _timelineService.GetEventsAsync("User1", DateTime.Now.AddDays(-2), DateTime.Now.AddDays(1));
+
+            // Assert
+            Assert.AreEqual(okResult.Count, result.Count);            
+        }
+
+        [Test]
+        public void GetEvents_401Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 401
+            };
+            _httpTest.RespondWithJson(apiResponse, 401);
+
+            // Assert
+            Assert.ThrowsAsync<UnauthorizedApiRequestException>(() => _timelineService.GetEventsAsync("User1", DateTime.Now.AddDays(-2), DateTime.Now.AddDays(1)));            
+        }
+
+        [Test]
+        public void GetEvents_400Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 400
+            };
+            _httpTest.RespondWithJson(apiResponse, 400);
+                                
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            // Assert
+            Assert.ThrowsAsync<InvalidApiRequestException>(() => _timelineService.GetEventsAsync("User1", start, end));            
+        }
+
+        [Test]
+        public void GetEvents_403Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 403
+            };
+            _httpTest.RespondWithJson(apiResponse, 403);
+                        
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            // Assert
+            Assert.ThrowsAsync<ForbiddenApiRequestException>(() => _timelineService.GetEventsAsync("User1", start, end));
+        }
+
+        [Test]
+        public void GetEvents_500Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 500
+            };
+            _httpTest.RespondWithJson(apiResponse, 500);
+                        
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            // Assert
+            Assert.ThrowsAsync<ApiException>(() => _timelineService.GetEventsAsync("User1", start, end));
+        }
+
+        [Test]
+        public void GetEvents_XMustBeBeforeY()
+        {
+            // Arrange           
+            var start = DateTimeOffset.Now;
+            var end = DateTimeOffset.Now.AddSeconds(-1);
+                       
+            // Assert            
+            var ex = Assert.ThrowsAsync<ApiWrapperException>(() => _timelineService.GetEventsAsync("User1", start, end));
+
+            Assert.True(ex.ExceptionDetails.Errors.ContainsKey("start"));
+            var startError = ex.ExceptionDetails.Errors["start"];
+            var endUtcFormatted = $"({end.ToString()})";
+            var startUtcFormatted = $"({start.ToString()})";
+            Assert.True(startError.Contains(string.Format(Constants.ErrorMessages.XMustBeBeforeY, startUtcFormatted, endUtcFormatted)));
+        }
+        
+        [Test]
+        public void GetEvents_XMustBeWithinYDaysOfZ()
+        {
+            // Arrange           
+            var start = DateTimeOffset.Now;
+            var end = DateTimeOffset.Now.AddDays(32).AddSeconds(1);
+
+            // Assert            
+            var ex = Assert.ThrowsAsync<ApiWrapperException>(() => _timelineService.GetEventsAsync("User1", start, end));
+
+            Assert.True(ex.ExceptionDetails.Errors.ContainsKey("end"));
+            var endError = ex.ExceptionDetails.Errors["end"];
+            var endUtcFormatted = $"({end.ToString()})";
+            var startUtcFormatted = $"({start.ToString()})";
+            Assert.True(endError.Contains(string.Format(Constants.ErrorMessages.XMustBeWithinYDaysOfZ, endUtcFormatted, 32, startUtcFormatted)));
+        }
+
+        [Test]
+        public async Task GetEvents_NoValidContent()
+        {
+            // Arrange            
+            var start = DateTimeOffset.Now;
+            var end = DateTimeOffset.Now.AddDays(2);
+
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 204
+            };
+
+            _httpTest.RespondWithJson(apiResponse, 204);
+
+            // act
+            var result = await _timelineService.GetEventsAsync("User1", start, end);
+
+            // Assert            
+            Assert.True(result.Count == 0);
+        }
+
+        #endregion
+
+        #region GetEvent tests
+        [Test]
+        public async Task GetEvent_OkResult()
+        {
+            // Arrange
+            var okResult = new List<TimelineEvent>
+            {
+                new TimelineEvent
+                {
+                    Id = "1",
+                    CreatedAt = DateTimeOffset.Now.AddDays(-1),
+                    End = DateTimeOffset.Now,
+                    EventType = "",
+                    InstanceId = "Id1",
+                    RepeatInstance = false,
+                    Repeats = false,
+                    RepeatType = "",
+                    RepeatUntil = null,
+                    Start = DateTimeOffset.Now.AddDays(-1),
+                    UpdatedAt = DateTimeOffset.Now.AddDays(-1)
+                }
+            };
+
+            _httpTest.RespondWithJson(okResult);
+
+
+            var result = await _timelineService.GetEventAsync("Event1");
+
+            // Assert
+            Assert.AreEqual(okResult.Count, result.Count);
+        }
+
+        [Test]
+        public void GetEvent_401Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 401
+            };
+            _httpTest.RespondWithJson(apiResponse, 401);
+
+            // Assert
+            Assert.ThrowsAsync<UnauthorizedApiRequestException>(() => _timelineService.GetEventAsync("Event1"));
+        }
+
+        [Test]
+        public void GetEvent_400Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 400
+            };
+            _httpTest.RespondWithJson(apiResponse, 400);
+
+            // Arrange           
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            // Assert
+            Assert.ThrowsAsync<InvalidApiRequestException>(() => _timelineService.GetEventAsync("Event1"));
+        }
+
+        [Test]
+        public void GetEvent_403Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 403
+            };
+            _httpTest.RespondWithJson(apiResponse, 403);
+                                  
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            // Assert
+            Assert.ThrowsAsync<ForbiddenApiRequestException>(() => _timelineService.GetEventAsync("Event1"));
+        }
+
+        [Test]
+        public void GetEvent_500Result()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 500
+            };
+            _httpTest.RespondWithJson(apiResponse, 500);
+                        
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddHours(23).AddMinutes(59).AddSeconds(59);
+
+            // Assert
+            Assert.ThrowsAsync<ApiException>(() => _timelineService.GetEventAsync("Event1"));
+        }
+               
+        [Test]
+        public async Task GetEvent_NoValidContent()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 204
+            };
+            _httpTest.RespondWithJson(apiResponse, 204);
+                        
+            // act
+            var result = await _timelineService.GetEventAsync("Event0");
+
+            // Assert            
+            Assert.True(result.Count == 0);            
+        }
+
+        [Test]
+        public async Task GetEvent_NoEventId()
+        {
+            // Arrange
+            var apiResponse = new ApiProblemDetails
+            {
+                Status = 204
+            };
+            _httpTest.RespondWithJson(apiResponse, 204);
+
+            // act
+            var result = await _timelineService.GetEventAsync("Event0");
+
+            // Assert            
+            Assert.ThrowsAsync<EmptyStringException>(() => _timelineService.GetEventAsync("")); 
+        }
+
+        #endregion
+
+        [TearDown]
+        public void DisposeHttpTest()
+        {
+            _httpTest.Dispose();
+        }
+    }
+}
---
+++ src/Updatedge.net/Services/V1/TimelineService.cs
@@ -0,0 +1,85 @@
+﻿using Flurl;
+using Flurl.Http;
+using Light.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Udatedge.Common.Models.TimelineEvents;
+using Udatedge.Common.Validation;
+using Updatedge.net.Exceptions;
+
+namespace Updatedge.net.Services.V1
+{
+    public class TimelineService : BaseService, ITimelineService
+    {
+        public TimelineService(string baseUrl, string apiKey) : base(baseUrl, apiKey)
+        {
+        }
+
+        public async virtual Task<List<TimelineEvent>> GetEventsAsync(string workerId, DateTimeOffset start, DateTimeOffset end)
+        {
+            try
+            {
+                // VALIDATION ------------------------------
+
+                var validator = new RequestValidator(
+                    new IntervalValidations(start, end)
+                        .StartEndSpecified()
+                        .LessThanXDays(32)
+                        .EndsAfterStart()
+                    );
+
+                if (validator.HasErrors) throw new ApiWrapperException(validator.ToDetails());
+
+                var response = await BaseUrl
+                    .AppendPathSegment($"timeline/{workerId}")
+                    .SetQueryParam("api-version", ApiVersion)
+                    .SetQueryParam("start", start.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"))
+                    .SetQueryParam("end", end.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"))                    
+                    .WithHeader(ApiKeyName, ApiKey)
+                    .GetAsync();
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    return new List<TimelineEvent>();
+                }
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<TimelineEvent>>(responseContent);
+            }
+            catch (FlurlHttpException flEx)
+            {
+                throw await flEx.Handle();
+            }
+        }
+
+        public async virtual Task<List<TimelineEvent>> GetEventAsync(string id)           
+        {
+            try 
+            {
+                // check that we have an Event Id.
+                id.MustNotBeNullOrEmpty("Please provide an Event Id.");
+                var response = await BaseUrl
+                    .AppendPathSegment($"timeline/event/{id}")
+                    .SetQueryParam("api-version", ApiVersion)
+                    .WithHeader(ApiKeyName, ApiKey)
+                    .GetAsync();
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    return new List<TimelineEvent>();
+                }
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<TimelineEvent>>(responseContent);
+                                
+            }
+            catch (FlurlHttpException flEx)
+            {
+                throw await flEx.Handle();
+            }
+        }
+    }
+}
