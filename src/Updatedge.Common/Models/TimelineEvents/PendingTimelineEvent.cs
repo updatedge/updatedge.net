0a68610408e1ed3b60e7f5260ba12da8d7c9d4a4
Steve Whitaker <stephen@rndthoughts.co.uk>
2020-09-16 09:06:49 +0100

    Adds pending work event submission api

---
+++ src/Updatedge.Common/Models/TimelineEvents/PendingTimelineEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Updatedge.Common.Models.TimelineEvents
+{
+    public class PendingTimelineEvent
+    {
+
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Start of event
+        /// </summary>
+        public DateTimeOffset Start { get; set; }
+
+        /// <summary>
+        /// End of event
+        /// </summary>
+        public DateTimeOffset End { get; set; }
+
+        /// <summary>
+        /// User Id of intended recipient
+        /// </summary>
+        public string RecipientUserId { get; set; }
+    }
+}
