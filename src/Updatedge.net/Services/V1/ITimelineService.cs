06791de5262f1191fe6b6b73f766df7645b33759
Steve Whitaker <stephen@rndthoughts.co.uk>
2020-09-17 02:01:30 +0100

    Last Shared and Get Max Availability

--- src/Updatedge.net/Services/V1/ITimelineService.cs
+++ src/Updatedge.net/Services/V1/ITimelineService.cs
@@ -32,5 +32,21 @@ namespace Updatedge.net.Services.V1
         /// <param name="pendingEvents">Events to be added</param>
         /// <returns>The new pending event set id</returns>
         Task<string> CreatePendingWorkingEventsAsync(string userId, List<PendingTimelineEvent> pendingEvents);
+        
+        /// <summary>
+        /// Retrieves the furthest point in time at which each user specified becomes available (when their inferred availability ends).   
+        /// </summary>
+        /// <param name="userIds">List of userids to return max availability for</param>
+        /// <returns>Max availability for each user id</returns>
+        Task<List<WorkerMaxAvailability>> GetMaxAvailabilityDatesAsync(List<string> userIds);
+
+        /// <summary>
+        /// Retrieves when the last shared date and time for a given set of users.
+        /// </summary>
+        /// <param name="userIds">List of user ids</param>
+        /// <returns>Last shared date/times for each user (or no entry if never shared)</returns>
+        Task<List<LastSharedDetail>> GetLastSharedOn(List<string> userIds);
+
+
     }
 }
