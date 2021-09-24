6933883ce58f5b2f0068c5d08eb69544962a984b
Rob Davey <robdavey555@hotmail.com>
2021-09-24 08:41:13 +0100

    Timesheet email url - Add Total Hours (#15)
    * suport new end point availability/preview
    
    * Update to WorkersAvailabilityUrlRequest
    
    * Changes to Availabilty URL Requests
    
    * Add worked hours to request

--- src/Updatedge.Common/Models/Workers/WorkerRated.cs
+++ src/Updatedge.Common/Models/Workers/WorkerRated.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 using Updatedge.Common.Models.TimelineEvents;
 
 namespace Updatedge.Common.Models.Workers
@@ -8,6 +9,11 @@ namespace Updatedge.Common.Models.Workers
         public int Rating { get; set; }
 
         public string WorkerId { get; set; }
+
+        public decimal TotalHours
+        {
+            get { return Events.Sum(span => span.Hours); }
+        }
 
         public ICollection<PendingTimelineEvent> Events { get; set; }
     }
