d996cd3b72e0d8ab55c0827cdfbbdf913f1c029e
Steve Whitaker <stephen@rndthoughts.co.uk>
2023-10-09 12:16:30 +0100

    Adds interface definition

--- src/Updatedge.net/Services/V1/IOrganisationService.cs
+++ src/Updatedge.net/Services/V1/IOrganisationService.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Updatedge.Common.Models.Organisation;
 
 namespace Updatedge.net.Services.V1
 {
     public interface IOrganisationService
     {
         Task<byte[]> GetExternalLogo(string domain);
+        Task UpdateRepresentedByAsync(List<RepresentationStatus> statuses);
         Task UploadExternalLogo(string domain, string base64Image);
     }
 }
