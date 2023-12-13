999f554ea938ce92e78f2174fee3b99658f0878b
celadrian6 <131877468+celadrian6@users.noreply.github.com>
2023-12-13 12:47:22 +0100

    1423 bs rating to ue (V2) (#57)
    * flattened option
    
    * matching interface change
    
    * Missing code
    
    * Filter out trailing slash if present
    
    * Augment WorkersAvailabilityUrlRequest
    Add SyncRatingsRequest
    
    * Augment WorkersAvailabilityUrlRequest
    Add SyncRatingsRequest
    
    * Extra fields for rating synchronisation
    
    * Name and Domain fields added for Worker rating of External Hirer
    
    ---------
    
    Co-authored-by: Adrian Jones <adrian.jones@4myschools.org>

--- src/Updatedge.Common/Models/Rating/SyncRatingsRequest.cs
+++ src/Updatedge.Common/Models/Rating/SyncRatingsRequest.cs
@@ -12,6 +12,21 @@ namespace Updatedge.Common.Models.Rating
         public string ExternalHirerId { get; set; }
 
         /// <summary>
+        /// Agency's name for hirer
+        /// </summary>
+        public string ExternalOrganisationName { get; set; }
+
+        /// <summary>
+        /// Web domain of organisation if known (used to retrieve logo)
+        /// </summary>
+        public string OrganisationDomain { get; set; }
+
+        /// <summary>
+        /// Email address of the hirer within the hirer organisation
+        /// </summary>
+        public string HirerEmail { get; set; }
+
+        /// <summary>
         /// The id of the agency that sent the timesheet data
         /// </summary>
         public string AgencyId { get; set; }
@@ -24,17 +39,17 @@ namespace Updatedge.Common.Models.Rating
         /// <summary>
         /// List of workers with ratings as stored by agency
         /// </summary>
-        public IEnumerable<WorkerRating> WorkerRatings { get; set; }
+        public IEnumerable<ExternalWorkerRating> WorkerRatings { get; set; }
 
         /// <summary>
         /// List of workers with ratings *of this hirer* as stored by agency
         /// </summary>
-        public IEnumerable<WorkerRating> HirerRatings { get; set; }
+        public IEnumerable<ExternalWorkerRating> HirerRatings { get; set; }
 
         /// <summary>
         /// Defines a worker rating relationship
         /// </summary>
-        public class WorkerRating
+        public class ExternalWorkerRating
         {
             /// <summary>
             /// User Id
@@ -42,9 +57,19 @@ namespace Updatedge.Common.Models.Rating
             public string WorkerId { get; set; }
 
             /// <summary>
+            /// Worker email address
+            /// </summary>
+            public string EmailAddress { get; set; }
+
+            /// <summary>
             /// Rating
             /// </summary>
             public int Rating { get; set; }
+
+            /// <summary>
+            /// Rating Timestamp (null indicates a brand new rating)
+            /// </summary>
+            DateTimeOffset? UpdatedAt { get; set; }
         }
     }
 }
