b3b275dd90dbcafdd223e93ad180647269d14d5e
Steve Whitaker <stephen@rndthoughts.co.uk>
2025-07-07 13:39:27 +0100

    Added ShortOfferId

--- src/Updatedge.Common/Models/Offer/CompleteOffer.cs
+++ src/Updatedge.Common/Models/Offer/CompleteOffer.cs
@@ -8,6 +8,11 @@ namespace Updatedge.Common.Models.Offer
     {
         public string Id { get; set; }
         public IEnumerable<string> WorkerIds { get; set; }
+
+        /// <summary>
+        /// Short ID of the offer used for external references
+        /// </summary>
+        public string ShortOfferId { get; set; }
         public decimal? TotalGrossPay { get; set; }
         public decimal? TotalGrossCharge { get; set; }
         public List<CreateOffer.OfferEvent> Events { get; set; }
